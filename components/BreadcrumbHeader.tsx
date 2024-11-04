9ec632fc6a4f6d1fcd3cf9c48977eb2e58bc6ffb
Joe Clark <weirdlookingjay@live.com>
2024-11-04 12:36:15 -0500

    Addded mobile view for the sidebar navigation

--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -3,12 +3,14 @@
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from './ui/breadcrumb';
+import { MobileSidebar } from './Sidebar';
 
 const BreadcrumbHeader = () => {
     const pathName = usePathname();
     const paths = pathName === "/" ? [""] : pathName.split("/");
     return (
         <div className="flex items-center flex-start">
+            <MobileSidebar />
             <Breadcrumb>
                 <BreadcrumbList>
                     {paths.map((path, index) => (
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,12 +4,15 @@ import {
     CoinsIcon,
     HomeIcon,
     Layers2Icon,
+    MenuIcon,
     ShieldCheckIcon,
 } from "lucide-react";
 import Logo from "./Logo";
 import Link from "next/link";
-import { buttonVariants } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import { usePathname } from "next/navigation";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { useState } from "react";
 
 const routes = [
     {
@@ -59,5 +62,41 @@ const DesktopSidebar = () => {
         </div>
     );
 };
+
+export function MobileSidebar() {
+    const [isOpen, setOpen] = useState(false);
+    const pathname = usePathname();
+    const activeRoute = routes.find(route => route.href.length > 0 && pathname.includes(route.href) || routes[0])
+
+    return <div className="block border-separate bg-background md:hidden">
+        <nav className="container flex items-center justify-between px-8">
+            <Sheet open={isOpen} onOpenChange={setOpen}>
+                <SheetTrigger asChild>
+                    <Button variant={"ghost"} size={"icon"}>
+                        <MenuIcon />
+                    </Button>
+                </SheetTrigger>
+                <SheetContent className="w-[400px] sm:w-[540px] space-y-4" side={"left"}>
+                    <Logo />
+                    <div className="flex flex-col gap-1">
+                        {routes.map(route => (
+                            <Link
+                                key={route.href}
+                                href={route.href}
+                                className={buttonVariants({
+                                    variant: activeRoute?.href === route.href ? "sidebarActiveItem" : "sidebarItem",
+                                })}
+                                onClick={() => setOpen(prev => !prev)}
+                            >
+                                <route.icon size={20} />
+                                {route.label}
+                            </Link>
+                        ))}
+                    </div>
+                </SheetContent>
+            </Sheet>
+        </nav>
+    </div>
+}
 
 export default DesktopSidebar;
