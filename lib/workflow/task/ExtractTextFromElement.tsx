1d1ce0121b531aa654621af4cc7daf2128e4474c
Joe Clark <weirdlookingjay@live.com>
2024-11-06 12:08:10 -0500

    Created another task: ExtractTextFromElementTask

---
+++ lib/workflow/task/ExtractTextFromElement.tsx
@@ -0,0 +1,28 @@
+import { TaskParamType, TaskType } from "@/types/task";
+import { LucideProps, TextIcon } from "lucide-react";
+
+export const ExtractTextFromElementTask = {
+    type: TaskType.EXTRACT_TEXT_FROM_ELEMENT,
+    label: "Extract text from element",
+    icon: (props: LucideProps) => <TextIcon {...props} className="stroke-rose-400" />,
+    isEntryPoint: false,
+    inputs: [
+        {
+            name: "Html",
+            type: TaskParamType.STRING,
+            required: true,
+        },
+        {
+            name: "Selector",
+            type: TaskParamType.STRING,
+            required: true,
+        },
+
+    ],
+    outputs: [
+        {
+            name: "Extracted text",
+            type: TaskParamType.STRING
+        }
+    ]
+}
