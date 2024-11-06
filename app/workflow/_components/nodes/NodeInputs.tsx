fa9d53a7c966dbad8a393729e4cedc115fb943c1
Joe Clark <weirdlookingjay@live.com>
2024-11-06 12:53:36 -0500

    Check for an active edge connection before creating a new one (Allow only 1 connection).

--- app/workflow/_components/nodes/NodeInputs.tsx
+++ app/workflow/_components/nodes/NodeInputs.tsx
@@ -25,6 +25,7 @@ export function NodeInput({
             {!input.hideHandle && (
                 <Handle
                     id={input.name}
+                    isConnectable={!isConnected}
                     type="target"
                     position={Position.Left}
                     className={cn(
