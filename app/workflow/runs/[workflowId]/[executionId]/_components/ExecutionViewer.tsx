319649f1958ec288b4aff35802858454de65bc70
Joe Clark <weirdlookingjay@live.com>
2024-11-08 09:48:27 -0500

    When we click one of the phases - load the phase details

--- app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
+++ app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { GetWorkflowExecutionWithPhases } from "@/actions/workflows/getWorkflowExecutionWithPhases";
+import { GetWorkflowPhaseDetails } from "@/actions/workflows/getWorkflowPhaseDetails";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -18,7 +19,7 @@ import {
     LucideIcon,
     WorkflowIcon,
 } from "lucide-react";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 type ExecutionDate = Awaited<ReturnType<typeof GetWorkflowExecutionWithPhases>>;
 
@@ -27,6 +28,8 @@ export default function ExecutionViewer({
 }: {
     initialData: ExecutionDate;
 }) {
+    const [selectedPhase, setSelectedPhase] = useState<string | null>(null)
+
     const query = useQuery({
         queryKey: ["execution", initialData?.id],
         initialData,
@@ -34,6 +37,14 @@ export default function ExecutionViewer({
         refetchInterval: (q) =>
             q.state.data?.status === WorkflowExecutionStatus.RUNNING ? 1000 : false,
     });
+
+    const phaseDetails = useQuery({
+        queryKey: ["phaseDetails", selectedPhase],
+        enabled: selectedPhase !== null,
+        queryFn: () => GetWorkflowPhaseDetails(selectedPhase!),
+    })
+
+    const isRunning = query.data?.status === WorkflowExecutionStatus.RUNNING;
 
     const duration = DatesToDruationString(
         query.data?.completedAt,
@@ -92,17 +103,25 @@ export default function ExecutionViewer({
                     {query.data?.phases.map((phase, index) => (
                         <Button
                             key={phase.id}
-                            variant={"ghost"}
+                            variant={selectedPhase === phase.id ? "secondary" : "ghost"}
                             className="w-full justify-between"
+                            onClick={() => {
+                                if (isRunning) return;
+                                setSelectedPhase(phase.id)
+                            }}
                         >
                             <div className="flex items-center gap-2">
                                 <Badge variant={"outline"}>{index + 1}</Badge>
                                 <p className="font-semibold">{phase.name}</p>
                             </div>
+                            <p className="text-xs text-muted-foreground">{phase.status}</p>
                         </Button>
                     ))}
                 </div>
             </aside>
+            <div className="flex w-full h-full">
+                <pre>{JSON.stringify(phaseDetails.data, null, 4)}</pre>
+            </div>
         </div>
     );
 }
